759a2af1db94c20584edf0c52fa38f79ecf5ab2a
Igor Octaviano <igoroctaviano@gmail.com>
2021-11-24 17:50:09 -0300

    Revert "Bidirectional + Ellipse "

--- src/annotations/markups/ellipse/id.js
+++
@@ -1,9 +0,0 @@
-const ELLIPSE_ID_PREFIX = "ellipse-";
-
-export const getEllipseId = (ellipseHandlesFeature) => {
-  return ELLIPSE_ID_PREFIX + ellipseHandlesFeature.getId();
-};
-
-export const getEllipseHandlesId = (ellipseFeature) => {
-  return ellipseFeature.getId().split(ELLIPSE_ID_PREFIX)[1];
-};
--- src/annotations/styles.js
+++
@@ -1,7 +0,0 @@
-export const stroke = {
-  color: '#3399CC'
-}
-
-export default {
-  stroke
-}
